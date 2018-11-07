d2ab611d57ee557320720c173b3ea90df5179e95
Emilio <emilio@Emilios-MacBook-Pro.local>
2018-11-06 23:28:14 -0300

    1

---
+++ e_utils/utils_eb.py
@@ -0,0 +1,8 @@
+import string
+
+def filename(s):
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    filename = ''.join(c for c in s if c in valid_chars)
+    filename = filename.replace(' ','_')
+    filename = filename[:255]
+    return filename
